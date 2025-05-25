6b45bcc7f11b89051937c4de09f24c58678790c3
edatunc <220202034@kocaeli.edu.tr>
2025-05-25 22:21:07 +0300

    web service implementation

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,31 @@
 import express from 'express';
-import cors from 'cors';
+  import cors from 'cors';
 
-const app = express();
-const PORT = 6000;
+  const app = express();
+  const PORT = 6000;
+  const express = require('express');
+  const dotenv = require('dotenv');
+  const userRoutes = require('./routes/user');
+  const discoverRoutes = require('./routes/discover');
+  const messageRoutes = require('./routes/message');
+  const settingsRoutes = require('./routes/settings');
+  const notificationRoutes = require('./routes/notification');
+  const authRoutes = require('./routes/auth');
 
-app.use(cors());
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
+  app.use('/users', userRoutes);
+  app.use('/', discoverRoutes);
+  app.use('/', messageRoutes);
+  app.use('/', settingsRoutes);
+  app.use('/', notificationRoutes);
+  app.use('/', authRoutes);
 
-app.get('/hello',(req, res) => {
-    const { name } = req.body;
-    res.json({message: `Merhaba, ${name}`}); 
-})
+  app.get('/hello',(req, res) => {
+      const { name } = req.body;
+      res.json({message: `Merhaba, ${name}`});
+  })
 
-app.listen(PORT,()=>{
-    console.log(`Listening on port:${PORT}`);
-})
+  app.listen(PORT,()=>{
+      console.log(`Listening on port:${PORT}`);
+  })
