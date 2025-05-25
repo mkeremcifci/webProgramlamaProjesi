cf769f567022b656913624ef6b06c1847a13fbb1
mkeremcifci <84402564+mkeremcifci@users.noreply.github.com>
2025-05-25 21:43:22 +0300

    Session cookie (#2)
    * feat: implement basic session handling
    
    * feat: implement basic session management

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session';
+
+import sessionConfig from './config/sessionConfig.js';
+import router from './routes/index.js';
 
 const app = express();
 const PORT = 6000;
 
-app.use(cors());
+
+app.use(cors({
+    credentials: true
+}));
+
+app.use(sessionConfig)
+
 app.use(express.json());
 
-app.get('/hello',(req, res) => {
-    const { name } = req.body;
-    res.json({message: `Merhaba, ${name}`}); 
-})
+app.use('/',router);
 
 app.listen(PORT,()=>{
     console.log(`Listening on port:${PORT}`);
